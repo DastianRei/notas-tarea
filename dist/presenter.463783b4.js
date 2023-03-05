e03ebe1563ca75856458bda6a3f57721ac98f94c
Dayan <dayan.aguilar@ucb.edu.bo>
2023-03-05 16:56:22 -0400

    Creamos la funcion de validad formulario

--- dist/presenter.463783b4.js
+++ dist/presenter.463783b4.js
@@ -118,17 +118,45 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"src/presenter.js":[function(require,module,exports) {
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
+function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 var listaNotas = [];
 var objNota = {
-  fecha: '',
-  titulo: '',
-  texto: ''
+  fecha: "",
+  titulo: "",
+  texto: ""
 };
 var editando = false;
-var formulario = document.querySelector('#formulario');
-var tituloInput = document.querySelector('#titulo');
-var textoInput = document.querySelector('#texto');
-var btnCrearInput = document.querySelector('#btn-crear');
+var formulario = document.querySelector("#formulario");
+var tituloInput = document.querySelector("#titulo");
+var textoInput = document.querySelector("#texto");
+var btnCrearInput = document.querySelector("#btn-crear");
+ormulario.addEventListener("submit", validarFormulario);
+function validarFormulario(e) {
+  e.preventDefault();
+  if (tituloInput.value === "" || textoInput.value === "") {
+    alert("Todos los campos se deben llenar");
+    return;
+  }
+  objNota.fecha = Date.now();
+  objNota.titulo = tituloInput.value;
+  objNota.texto = textoInput.value;
+  agregarEmpleado();
+}
+function agregarEmpleado() {
+  listaNotas.push(_objectSpread({}, objNota));
+  formulario.reset();
+  limpiarObjeto();
+}
+function limpiarObjeto() {
+  objEmpleado.id = '';
+  objEmpleado.nombre = '';
+  objEmpleado.puesto = '';
+}
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,41 @@
 let listaNotas = [];
 
 const objNota = {
-    fecha: '',
-    titulo: '',
-    texto: ''
-}
+  fecha: "",
+  titulo: "",
+  texto: "",
+};
 
 let editando = false;
 
-const formulario = document.querySelector('#formulario');
-const tituloInput = document.querySelector('#titulo');
-const textoInput = document.querySelector('#texto');
-const btnCrearInput = document.querySelector('#btn-crear');
+const formulario = document.querySelector("#formulario");
+const tituloInput = document.querySelector("#titulo");
+const textoInput = document.querySelector("#texto");
+const btnCrearInput = document.querySelector("#btn-crear");
 
+ormulario.addEventListener("submit", validarFormulario);
+
+function validarFormulario(e) {
+  e.preventDefault();
+
+  if (tituloInput.value === "" || textoInput.value === "") {
+    alert("Todos los campos se deben llenar");
+    return;
+  }
+
+  objNota.fecha = Date.now();
+  objNota.titulo = tituloInput.value;
+  objNota.texto = textoInput.value;
+  agregarEmpleado();
+}
+
+function agregarEmpleado() {
+  listaNotas.push({ ...objNota });
+  formulario.reset();
+  limpiarObjeto();
+}
+function limpiarObjeto() {
+    objEmpleado.id = '';
+    objEmpleado.nombre = '';
+    objEmpleado.puesto = '';
+}
